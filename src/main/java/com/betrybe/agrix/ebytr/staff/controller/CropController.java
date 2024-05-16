53eff5b658fcf905552f182d1dd932628b412ce5
Valber <valber_558@hotmail.com>
2024-05-16 16:41:46 -0300

    Requisito-05

--- src/main/java/com/betrybe/agrix/ebytr/staff/controller/CropController.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/CropController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,7 @@ public class CropController {
    * Get all Crops.
    */
   @GetMapping
+  @PreAuthorize("hasAuthority('ROLE_ADMIN') or hasAuthority('ROLE_MANAGER')")
   public List<CropDto> getAllCrops() {
     List<Crop> allCrops = cropService.findAll();
     return allCrops.stream()
